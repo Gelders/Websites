8a6e6031e43b5d7b2c8757b6cc96b16541330b31
Peter Gelders <42740552+Gelders@users.noreply.github.com>
2019-08-02 22:19:21 +0200

    Add files via upload

---
+++ Links - voor websites (HANDIG)/Color For Web Design.bat
@@ -0,0 +1,4 @@
+@echo off
+start chrome https://www.materialui.co/
+start http://paletton.com/#uid=1000u0kllllaFw0g0qFqFg0w0aF
+start https://www.design-seeds.com/
---
+++ Links - voor websites (HANDIG)/Web Design LINKS.bat
@@ -0,0 +1,7 @@
+@echo off
+start chrome https://iconmonstr.com/popular-collections/
+start http://wpflex.grayghostvisuals.com/
+start https://speckyboy.com/top-50-free-icon-sets/
+start https://www.creativebloq.com/web-design/free-icon-sets-10134829
+start https://www.webdesignerdepot.com/2017/04/15-essential-free-icon-sets/
+start http://www.bgpatterns.com/
